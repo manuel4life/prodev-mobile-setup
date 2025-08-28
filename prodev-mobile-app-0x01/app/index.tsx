f7044e1f9c6ca53e04dcc63cf18ae45d9a0fdee8
manuel4life <allotey4eva@gmail.com>
2025-08-28 17:39:04 +0200

    corrected files

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -8,11 +8,11 @@ export default function Index() {
       <Text style={styles.largeText}>
         Typescript is great if you practice more
       </Text>
+      \r\n{" "}
       <Text style={styles.mediumText}>
         React Native provides you a single codebase for cross platforms
       </Text>
-      <Text style={styles.smallText}>ALX is awesome</Text>
-
+      \r\n <Text style={styles.smallText}>ALX is awesome</Text>
       {/* Additional Text Components */}
       <Text style={styles.boldText}>Learning never stops</Text>
       <Text style={styles.italicText}>Practice makes perfect</Text>
